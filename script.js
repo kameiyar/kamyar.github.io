62730c5cb3153cdae57ac0ffb2ef2a5e67558a16
kameiyar <ahmad1250wxdadzh@gmail.com>
2025-02-11 20:01:49 +0330

    script.js

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
-// نمایش ساعت دیجیتالی
+// انیمیشن لودینگ
+window.onload = function() {
+    document.getElementById("preloader").style.display = "none";
+};
+
+// ساعت ایران
 function updateClock() {
     const now = new Date();
-    let hours = now.getHours().toString().padStart(2, '0');
-    let minutes = now.getMinutes().toString().padStart(2, '0');
-    let seconds = now.getSeconds().toString().padStart(2, '0');
-
-    document.getElementById('clock').innerText = `${hours}:${minutes}:${seconds}`;
+    const options = { timeZone: 'Asia/Tehran', hour: '2-digit', minute: '2-digit', second: '2-digit' };
+    document.getElementById('clock').innerText = now.toLocaleTimeString('fa-IR', options);
 }
-
-// به‌روزرسانی ساعت هر ثانیه
 setInterval(updateClock, 1000);
-
-// نمایش ساعت بلافاصله بعد از بارگذاری صفحه
 updateClock();
+
+// تاریخ شمسی، میلادی و قمری
+function updateDate() {
+    const now = new Date();
+    document.getElementById('jalali-date').innerText = new Intl.DateTimeFormat('fa-IR').format(now);
+    document.getElementById('gregorian-date').innerText = new Intl.DateTimeFormat('en-GB').format(now);
+    document.getElementById('hijri-date').innerText = new Intl.DateTimeFormat('ar-TN-u-ca-islamic').format(now);
+}
+updateDate();
