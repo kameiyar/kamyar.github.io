dafcf087b6c5c524fe450d9a867d4a1fb8fc74c7
kameiyar <ahmad1250wxdadzh@gmail.com>
2025-02-11 07:51:57 +0330

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-https://kameiyar.github.io/kamyar.github.io/
+// نمایش شماره تلفن
+function showNumber() {
+    const phoneNumber = "+989399743277";
+    document.getElementById('phone-number').innerText = phoneNumber;
+}
+
+// نمایش ساعت لحظه‌ای
+function updateTime() {
+    const now = new Date();
+    const hours = now.getHours().toString().padStart(2, '0');
+    const minutes = now.getMinutes().toString().padStart(2, '0');
+    const seconds = now.getSeconds().toString().padStart(2, '0');
+    document.getElementById('clock').innerText = `${hours}:${minutes}:${seconds}`;
+}
+
+setInterval(updateTime, 1000);
